7a3f2238fd21214df5f30b1cd8673118f6e19b29
Siyamthanda <ludongasiya@gmail.com>
2020-09-16 16:40:39 +0200

    my intial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello guys! How are you doing?")
+print("Awe Mase Kind")
+print(5+9)
+
+firstname=input("What is your name\n")
+print("my name is ", firstname)
+#====================AREA OF A CIRCLE================
+r = int(input("Please enter the radius\n"))
+pi = 3.14
+area_of_a_circle=pi * r * r
+print("The area of the circle with radius" , area_of_a_circle)
+
